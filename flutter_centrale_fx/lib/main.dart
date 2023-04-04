0f6efcaf1c2686404b2ffba16110c0fe347a2487
alvachon <alvachon@student.42quebec.com>
2023-04-03 21:28:24 -0400

    Update main.dart

--- flutter_centrale_fx/lib/main.dart
+++ flutter_centrale_fx/lib/main.dart
@@ -5,7 +5,8 @@ void main()
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget
+{
   const MyApp({super.key});
 
   // This widget is the root of your application.
@@ -50,11 +51,13 @@ class MyHomePage extends StatefulWidget
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+{
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
+  void _incrementCounter()
+  {
+    setState((){
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
